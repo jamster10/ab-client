ed09d8bdd3069cf1e5234e7bbd2110f8f22d1585
Kristof-Pierre Cummings <kristofpierre8@gmail.com>
2019-07-30 09:49:55 -0500

    updated description. Testing out useMemo

--- src/components/BookingTable/BookingTable.js
+++ src/components/BookingTable/BookingTable.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { TableRow } from '../TableRow/TableRow'
 import { Table, Thead, Tbody, Tr, Th } from 'react-super-responsive-table'
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
-
+const generateTableRows = bookings => {console.log( 'ran');return bookings.map( booking => <TableRow  key ={booking.created_at}booking={booking}/>)}
 export default function BookingTable({bookings = []}) {
   
-  const generateTableRows = bookings => bookings.map( booking => <TableRow  key ={booking.created_at}booking={booking}/>)
+
+  const genTableRows = useMemo(() => generateTableRows(bookings), [bookings])
 
   return (
     <Table>
@@ -19,7 +20,7 @@ export default function BookingTable({bookings = []}) {
         </Tr>
       </Thead>
       <Tbody>
-        {generateTableRows(bookings)}
+        {genTableRows}
       </Tbody>
     </Table>
   )
